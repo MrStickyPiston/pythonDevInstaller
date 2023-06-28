0cc3202abf7bf7f586eea27d0aad48c761e51a51
sticky <mr.sticky.piston@gmail.com>
2023-06-28 10:23:17 +0200

    Fixed progress bars

--- installer.py
+++ installer.py
@@ -34,21 +34,24 @@ FIREFOX_URL = "https://download.mozilla.org/?product=firefox-latest-ssl&os=win64
 
 # DOWNLOAD
 def download_executables():
-    def progressBar(count_value, block_size, total, filler='◼'):
-        percent = count_value/total*block_size
-        sys.stdout.write(f"\r{int(percent*10)*filler}{int(percent*10)}")
+    def progress_bar(count_value, block_size, total, size=20, filled='█', empty='░'):
+        done = count_value/total*block_size
+        sys.stdout.write(f"\r[{round(done*100)}%] {int(done*size)*filled}{(size-int(done*size))*empty}")
+
+        if done >= 1:
+            sys.stdout.write("\n")
 
     logging.info("Downloading Python")
-    urllib.request.urlretrieve(PYTHON_URL, f"{exec_dir}\\python_setup.exe", progressBar)
+    urllib.request.urlretrieve(PYTHON_URL, f"{exec_dir}\\python_setup.exe", progress_bar)
 
     logging.info("Downloading Pycharm")
-    urllib.request.urlretrieve(PYCHARM_URL, f"{exec_dir}\\pycharm_setup.exe", progressBar)
+    urllib.request.urlretrieve(PYCHARM_URL, f"{exec_dir}\\pycharm_setup.exe", progress_bar)
 
     logging.info("Downloading Git")
-    urllib.request.urlretrieve(GIT_URL, f"{exec_dir}\\git_setup.exe", progressBar)
+    urllib.request.urlretrieve(GIT_URL, f"{exec_dir}\\git_setup.exe", progress_bar)
 
     logging.info("Downloading Firefox")
-    urllib.request.urlretrieve(FIREFOX_URL, f"{exec_dir}\\firefox_setup.exe", progressBar)
+    urllib.request.urlretrieve(FIREFOX_URL, f"{exec_dir}\\firefox_setup.exe", progress_bar)
 
 
 if __name__ == "__main__":
